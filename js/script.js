8d276c02725bdabfaec5f32a11369ed48b468a90
hoang Tu <mrahn1234@gmail.com>
2021-06-07 00:41:01 +0700

    'init_project

---
+++ js/script.js
@@ -0,0 +1,8 @@
+open_menu_btn  = document.getElementById('open_mobile_menu');
+open_menu_btn.onclick = () => {onToggleMenu()};
+
+// var onToggleMenu = () => {
+//     var bg_mobile_menu = document.getElementsByClassName('header_bg')[0];
+//     bg_mobile_menu.style.zIndex = 999;
+//     bg_mobile_menu.style.width = "100%"
+// }
